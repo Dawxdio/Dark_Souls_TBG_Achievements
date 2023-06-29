851062de36a7cb246b2ade8b0dfc6d4e5c3a97a4
Dawxdio <dawidkujawski04@gmail.com>
2023-06-29 10:38:17 +0200

    ikonki i lepsze opisy achievementow

--- dstbg_achievements/lib/main.dart
+++ dstbg_achievements/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
-void main() async{
+
+void main() async {
   runApp(const MyApp());
 }
 
@@ -13,30 +14,23 @@ void saveProgress() async {
   await file.writeAsString(jsonEncode(achievementDesc));
 }
 
-var template;
-var achievementDesc;
-loadTemplate() async
-{
+dynamic template;
+dynamic achievementDesc;
+loadTemplate() async {
   String response;
   response = await rootBundle.loadString("assets/Templates/template2.json");
   template = jsonDecode(response);
-  achievementDesc=template;
+  achievementDesc = template;
 }
 
 loadProgress() async {
   final directory = await getApplicationDocumentsDirectory();
-  var file;
-  
-  if(await File('${directory.path}/progress.json').exists())
-  {
-    file = File('${directory.path}/progress.json');
+  if (await File('${directory.path}/progress.json').exists()) {
+    File file = File('${directory.path}/progress.json');
     String read = await file.readAsString();
     final result = jsonDecode(read);
     achievementDesc = result;
-  }
-  else
-  {
-    print("s");
+  } else {
     await loadTemplate();
   }
 }
@@ -52,28 +46,30 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: loadProgress(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: Scaffold(
-                  body: Main(),
-                ));
-          } else {
-            return const MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: Scaffold(body: CircularProgressIndicator()));
-          }
-        });
+      future: loadProgress(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Main(),
+            ),
+          );
+        } else {
+          return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(body: CircularProgressIndicator()));
+        }
+      },
+    );
   }
 }
 
-bool complete = false;
+// bool complete = false;
 
 class Main extends StatefulWidget {
   const Main({super.key});
- 
+
   @override
   State<Main> createState() => _MainState();
 }
@@ -81,20 +77,25 @@ class Main extends StatefulWidget {
 class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
+
+    AssetImage whatImage(n, i){
+      String result = n[i];
+      result = result.toString().toLowerCase();
+      result+=".png";
+      return AssetImage("assets/Icons/$result");
+    }
+
     int howManyDone() {
       int count = 0;
-      
-      print(achievementDesc["template"]);
       for (int i = 0; i < achievementDesc["template"].length; i++) {
         if (achievementDesc["template"][i]["finished"]) {
           count++;
         }
       }
-      print(achievementDesc["template"].length);
       return count;
     }
 
-    void sort(){
+    void sort() {
       /*if(complete){
         achievementNames.sort((a,b){
         String aValue = achievementDesc[a]![1].toString();
@@ -110,120 +111,135 @@ class _MainState extends State<Main> {
       }
       */
     }
-    void deleteSave() async
-    {
+    void deleteSave() async {
       final directory = await getApplicationDocumentsDirectory();
       final file = File('${directory.path}/progress.json');
       await file.delete();
     }
+
     void reset() async {
       await showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text("Reset All Achievement Progress?"),
-              actions: [
-                IconButton(
-                    onPressed: () {
-                      setState((){
-                        deleteSave();
-                        for (int i = 0; i < achievementDesc["template"].length; i++) {
-                          achievementDesc["template"][i]["finished"] = false;
-                        }
-                        //saveProgress();
-                        Navigator.pop(context);
-                      });
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Reset All Achievement Progress?"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      deleteSave();
+                      for (int i = 0;
+                          i < achievementDesc["template"].length;
+                          i++) {
+                        achievementDesc["template"][i]["finished"] = false;
+                      }
+                      //saveProgress();
+                      Navigator.pop(context);
                     },
-                    icon: const Icon(
-                      Icons.check,
-                      size: 30,
-                    )),
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        Navigator.pop(context);
-                      });
+                  );
+                },
+                icon: const Icon(
+                  Icons.check,
+                  size: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      Navigator.pop(context);
                     },
-                    icon: const Icon(
-                      Icons.cancel,
-                      size: 30,
-                    ))
-              ],
-            );
-          });
+                  );
+                },
+                icon: const Icon(
+                  Icons.cancel,
+                  size: 30,
+                ),
+              ),
+            ],
+          );
+        },
+      );
     }
 
     int done = howManyDone();
-    var ez="ez";
-    return Column(children: [
-      Expanded(
-        flex: 2,
-        child: Container(
-          color: Colors.black,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(10, 48, 0, 10),
-                child: Text(
-                  "Achievements",
-                  style: TextStyle(fontSize: 36, color: Colors.white),
+    return Column(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 48, 0, 10),
+                  child: Text(
+                    "Achievements",
+                    style: TextStyle(fontSize: 36, color: Colors.white),
+                  ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Progress: $done / ${achievementDesc["template"].length}",
-                      style: const TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                    IconButton(
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Progress: $done / ${achievementDesc["template"].length}",
+                        style:
+                            const TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      IconButton(
                         onPressed: () {
-                          setState(() {
-                            reset();
-                          });
+                          setState(
+                            () {
+                              reset();
+                            },
+                          );
                         },
                         icon: const Icon(
                           Icons.restart_alt_outlined,
                           color: Colors.white,
-                        )),
-                    IconButton(
+                        ),
+                      ),
+                      IconButton(
                         onPressed: () {
-                          setState(() {
-                            sort();
-                            complete = !complete;
-                          });
+                          setState(
+                            () {
+                              // sort();
+                              // complete = !complete;
+                            },
+                          );
                         },
                         icon: const Icon(
                           Icons.sort,
                           color: Colors.white,
-                        ))
-                  ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              LinearProgressIndicator(
-                value: done / achievementDesc["template"].length,
-                minHeight: 10,
-                valueColor: const AlwaysStoppedAnimation(Colors.red),
-                backgroundColor: Colors.grey[900],
-              ),
-            ],
+                LinearProgressIndicator(
+                  value: done / achievementDesc["template"].length,
+                  minHeight: 10,
+                  valueColor: const AlwaysStoppedAnimation(Colors.red),
+                  backgroundColor: Colors.grey[900],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      
-        
-      Expanded(
+        Expanded(
           flex: 7,
           child: Container(
             height: MediaQuery.of(context).size.height,
             decoration: const BoxDecoration(
-              image:DecorationImage(image: AssetImage("assets/wallpaper.jpg"),fit: BoxFit.fill,)
-              
-            ),
+                image: DecorationImage(
+              image: AssetImage("assets/wallpaper.jpg"),
+              fit: BoxFit.fill,
+            )),
             child: ListView.builder(
               shrinkWrap: true,
               itemCount: achievementDesc["template"].length,
@@ -247,7 +263,6 @@ class _MainState extends State<Main> {
                               style: const TextStyle(
                                   color: Colors.white, fontSize: 24),
                             ),
-                            
                             Padding(
                               padding: const EdgeInsets.fromLTRB(8, 6, 0, 20),
                               child: Text(
@@ -257,44 +272,48 @@ class _MainState extends State<Main> {
                                     color: Colors.white, fontSize: 18),
                               ),
                             ),
-                            const Padding(
-                              padding: EdgeInsets.fromLTRB(8, 6, 0, 20),
-                              child: Image(
-                                image: AssetImage("assets/Icons/death.png"),
-                              height: 70,
+                            Row(
+                              children: [
+                                for(int i=0;i<achievementDesc["template"][index]["type"].length;i++) //tak, to działa i jestem z tego powodu bardzo zadowolony :3
+                                  Padding(
+                                  padding: const EdgeInsets.fromLTRB(8, 2, 0, 20),
+                                  child: Image(
+                                    image: whatImage(achievementDesc["template"][index]["type"], i),
+                                    height: 70,
+                                  ),
+                                ),
+                              ],
                             ),
-                            ),
-                            
                           ],
                         ),
                       ),
                       Transform.scale(
                         scale: 1.3,
                         child: Checkbox(
-                            fillColor: MaterialStateProperty.all(Colors.white),
-                            checkColor: Colors.red,
-                            materialTapTargetSize: MaterialTapTargetSize.padded,
-                            value: achievementDesc["template"][index]["finished"],
-                            onChanged: (bool? value) {
-                              setState(() {
+                          fillColor: MaterialStateProperty.all(Colors.white),
+                          checkColor: Colors.red,
+                          materialTapTargetSize: MaterialTapTargetSize.padded,
+                          value: achievementDesc["template"][index]["finished"],
+                          onChanged: (bool? value) {
+                            setState(
+                              () {
                                 achievementDesc["template"][index]["finished"] =
                                     value;
                                 saveProgress();
-                              });
-                            }),
+                              },
+                            );
+                          },
+                        ),
                       ),
-                      
-                      
                     ],
                   ),
-                  
-            );
-            
-                      },
-                    ),
-          )),
-    
-    ]);
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 /*⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣤⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣶⡆
